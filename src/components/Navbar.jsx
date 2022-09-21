84c0f08ef3e05b1b06f4329b7381feb2521ae96b
karagwe <metaexlore@gmail.com>
2022-09-22 01:56:53 +0300

    added create profile url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,35 @@ import { Box, Button, Text } from '@chakra-ui/react'
 import React from 'react'
 import { AiOutlineUserAdd } from 'react-icons/ai'
 import { BsArrowRight } from 'react-icons/bs'
+import { GiGroupedDrops } from 'react-icons/gi'
 
 export default function Navbar() {
   return (
     <Box display="flex" justifyContent="space-between"  alignItems="center" h={75}  py={7} px={5} maxWidth="1400px">
         <Box>
-            <Text fontSize="lg" cursor="pointer">Pax-Fi</Text>
+           {/*} <Text fontSize="lg" cursor="pointer">Pax-Fi</Text>*/}
+           <GiGroupedDrops  size={40} color="#6874F2" cursor="pointer"/>
         </Box>
         <Box>
-            <Button leftIcon={<AiOutlineUserAdd  />} rightIcon= {<BsArrowRight  />} color="blackAlpha.900" size="lg" mr={6}>Create  Profile</Button>
-             <Button rightIcon={<BsArrowRight />} color="whiteAlpha.900" size="lg" bgColor="#5865F2">Launch this  App </Button>
+            <Button leftIcon={<AiOutlineUserAdd  />} rightIcon= {<BsArrowRight  />} color="blackAlpha.900" size="lg" mr={6}>
+            <a
+        target="_blank"
+         rel="noreferrer"
+        href='https://testnet.lenster.xyz/'>
+        Create  Profile
+</a>
+              
+              
+              </Button>
+             <Button rightIcon={<BsArrowRight />} color="whiteAlpha.900" size="lg" bgColor="#5865F2">
+             <a
+        target="_blank"
+         rel="noreferrer"
+        href='https://pax-fi.vercel.app/'>
+          Launch App 
+         </a>
+             
+              </Button>
         </Box>
     </Box>
   )
